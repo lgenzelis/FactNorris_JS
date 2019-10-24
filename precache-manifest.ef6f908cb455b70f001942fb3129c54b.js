4c9b9b82eb818da71a44450b1492c41bab1be8ea
lgenzelis <lucas.genzelis@gmail.com>
2019-10-24 20:01:45 -0300

    Updates

---
+++ precache-manifest.ef6f908cb455b70f001942fb3129c54b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578f21910d91e48a4cc8b6a1732d1e14",
+    "url": "/chuckfacts/index.html"
+  },
+  {
+    "revision": "073e9a7aa1d865d7992d",
+    "url": "/chuckfacts/static/css/main.ee2d9d77.chunk.css"
+  },
+  {
+    "revision": "38152fc06f9096f16ddf",
+    "url": "/chuckfacts/static/js/2.e1271ae5.chunk.js"
+  },
+  {
+    "revision": "073e9a7aa1d865d7992d",
+    "url": "/chuckfacts/static/js/main.54aa7200.chunk.js"
+  },
+  {
+    "revision": "c1bcef4c9479bd988fe9",
+    "url": "/chuckfacts/static/js/runtime~main.41d56ed0.js"
+  }
+]);
